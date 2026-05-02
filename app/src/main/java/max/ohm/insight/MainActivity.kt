c937fe6bc94811680a018410f3f20446ab2728e4
maxohm1 <maxohm24@gmail.com>
2026-05-03 00:11:08 +0530

    update main activity to display insights screen

---
+++ app/src/main/java/max/ohm/insight/MainActivity.kt
@@ -0,0 +1,46 @@
+package max.ohm.insight
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import max.ohm.insight.ui.theme.InsightTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            InsightTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    androidx.compose.foundation.layout.Box(modifier = Modifier.padding(innerPadding)) {
+                        max.ohm.insight.ui.screens.InsightsScreen()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    InsightTheme {
+        Greeting("Android")
+    }
+}
